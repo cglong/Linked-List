d0844fcc249161d317341d6086309511d63b5d07
Chris Long <christopherglong@mac.com>
2011-04-12 19:16:37 -0400

    remove_data

--- list.c
+++ list.c
@@ -173,8 +173,31 @@ int pop_back(list* llist, list_op free_func)
   */
 int remove_data(list* llist, void* data, compare_op compare_func, list_op free_func)
 {
-    /// @todo Implement changing the return value!
-    /// @note remember to also free all nodes you remove.
+    node *head = llist->head;
+    node *test = head;
+    node *prev, *next;
+    
+    do {
+        prev = test->prev;
+        next = test->next;
+        if (compare_func(test->data, data)) {
+            prev->next = next;
+            next->prev = prev;
+            
+            if (test == head)
+                llist->head = next;
+            
+            free_func(test->data);
+            free(test);
+            
+            if (test != NULL)
+                return -1;
+        }
+        test = next;
+    } while (test != head);
+    
+    // FIXME Handle last node case
+    
     return 0;
 }
 
