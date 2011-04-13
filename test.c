58af8a92febf04f28a3ba6d7baa577a01796280c
Chris Long <christopherglong@mac.com>
2011-04-13 01:19:17 -0400

    More test cases

--- test.c
+++ test.c
@@ -40,12 +40,10 @@ int compare_point_data(const void* a, const void* b)
 	int pasum = pa->x + pa->y;
 	int pbsum = pb->x + pb->y;
 	
-	if (pasum < pbsum)
-	    return -1;
-	else if (pasum == pbsum)
-	    return 0;
-	else
+	if (pasum == pbsum)
 	    return 1;
+	else
+	    return 0;
 }
 
 /* This main function does a little testing
@@ -102,6 +100,44 @@ int main(void)
  	printf("TEST CASE 7\nAfter freeing all nodes the list should be empty:");
  	traverse(llist, print_point);
  	printf("\n");
+ 	
+ 	/* Test remove_data */
+ 	point *p8 = create_point(3, 5);
+ 	push_front(llist, p8);
+ 	push_front(llist, create_point(9, 16));
+ 	remove_data(llist, p8, compare_point_data, free_point);
+ 	printf("TEST CASE 8\nAfter removal only one should remain:");
+ 	traverse(llist, print_point);
+ 	printf("\n");
+ 	pop_back(llist, free_point);
+ 	
+ 	/* List querying */
+ 	push_front(llist, create_point(3, 5));
+ 	push_front(llist, create_point(9, 16));
+ 	printf("TEST CASE 9");
+ 	printf("\nFront:");
+ 	print_point(front(llist));
+ 	printf("\nBack:");
+ 	print_point(back(llist));
+ 	printf("\nIs empty: %d", is_empty(llist));
+ 	printf("\nSize: %d", size(llist));
+ 	printf("\n");
+ 	pop_front(llist, free_point);
+ 	pop_front(llist, free_point);
+ 	
+ 	/* Searching */
+ 	point *p10 = create_point(3, 5);
+ 	push_front(llist, p10);
+ 	push_front(llist, create_point(9, 16));
+ 	int result = find_occurrence(llist, p10, compare_point_data);
+ 	printf("TEST CASE 10\nShould be true: %d", result);
+ 	p10 = create_point(4, 5);
+ 	result = find_occurrence(llist, p10, compare_point_data);
+ 	printf("\nShould be false: %d", result);
+ 	printf("\n");
+ 	free_point(p10);
+ 	pop_front(llist, free_point);
+ 	pop_front(llist, free_point);
  	
  	/* Testing over clean up*/
  	free(llist);
